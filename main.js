a4702185f7610b4affadc432439a3aba3fe0b35b
clstb <claas.stoertenbecker@gmail.com>
2019-02-03 13:19:28 +0100

    add comments

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-let fs = require('fs');
-let ffmpeg = require('fluent-ffmpeg');
-let yargs = require('yargs');
-let chalk = require('chalk');
+let fs = require('fs'); // filesystem library
+let ffmpeg = require('fluent-ffmpeg'); // ffmpeg wrapper library
+let yargs = require('yargs'); // command line arguments library
+let chalk = require('chalk'); // colored console output library
 
 yargs.options({
+    // base video file
     'b': {
         alias: 'base',
         describe: 'base video file',
@@ -11,6 +12,7 @@ yargs.options({
         nargs: 1,
         demand: true,
     },
+    // encoded video file
     'e': {
         alias: 'encoded',
         describe: 'encoded video file',
@@ -18,58 +20,127 @@ yargs.options({
         nargs: 1,
         demand: true,
     },
-    'l': {
-        alias: 'logPath',
-        describe: 'path to store logs',
+    // verbosity level
+    'v': {
+        alias: 'verbose',
+        describe: 'set verbosity of log output',
+        type: 'boolean',
+        nargs: 1,
+        default: false,
+    },
+    // vmaf log path
+    'lv': {
+        alias: 'vmaf_log',
+        describe: 'path to store vmaf logs',
         type: 'string',
         nargs: 1,
-        demand: true,
+        demand: false,
+        default: '',
+    },
+    // psnr log path
+    'lp': {
+        alias: 'psnr_log',
+        describe: 'path to store psnr logs',
+        type: 'string',
+        nargs: 1,
+        demand: false,
+        default: '',
     }
 });
 
-run(yargs.argv.base, yargs.argv.encoded, yargs.argv.logPath);
-
-function run(base, encoded, logPath) {
-    let filter = 'libvmaf=model_path=/usr/share/model/vmaf_v0.6.1.pkl:psnr=1:log_fmt=json:log_path=%s'.replace('%s', logPath);
-    let command = ffmpeg();
-    command.on('start', function(commandLine) {
-            console.debug('Spawned Ffmpeg with command: ' + chalk.default.blue(commandLine));
-        })
-        .on('progress', function(progress) {
-            process.stdout.clearLine();
-            process.stdout.cursorTo(0);
-            process.stdout.write('Processing: %d % done'.replace('%d', progress.percent));
-        })
-        .on('error', function(err) {
-            console.log('An error occurred: ' + err.message);
-        })
-        .on('end', function() {
-            console.log('Processing finished.');
-            let metrics = processLog(logPath);
-            console.log(metrics);
-        })
-        .input(base)
-        .input(encoded)
-        .addOption('-lavfi')
-        .addOption(filter)
-        .addOption('-f', 'null')
-        .output('-')
-        .run();
+// if psnr log path is set the psnr filter is executed
+if (yargs.argv.psnr_log != '') {
+    psnr(yargs.argv.base, yargs.argv.encoded, yargs.argv.psnr_log, yargs.argv.verbose);
 }
 
-function processLog(path) {
-    var data = fs.readFileSync(path);
-    var log = JSON.parse(data);
+// if vmaf log path is set the vmaf filter is executed
+if (yargs.argv.vmaf_log != '') {
+    vmaf(yargs.argv.base, yargs.argv.encoded, yargs.argv.vmaf_log, yargs.argv.verbose);
+}
 
-    var vmaf = 0;
-    var psnr = 0;
-    for(var frame of log.frames) {
+getResolution(yargs.argv.base);
+getResolution(yargs.argv.encoded);
+
+function getResolution(file) {
+    ffmpeg.ffprobe(file, function(err , metadata){
+        console.dir(metadata);
+    });
+}
+
+// vmaf runs the ffmpeg vmaf filter command
+function vmaf(base, encoded, logPath, verbose) {
+    // build filter string
+    let filter = 'libvmaf=model_path=/usr/share/model/vmaf_v0.6.1.pkl:log_fmt=json:log_path=%s'.replace('%s', logPath);
+    // create raw command
+    let command = ffmpeg();
+    // define command and run
+    command.on('start', function (commandLine) { // executes when the command starts
+        console.debug('Spawned FFmpeg with command:\n' + chalk.default.blue(commandLine));
+    })
+        .on('progress', function (progress) { // executes when the command progresses
+            if (verbose === true) {
+                console.log('Processing VMAF: %d % done'.replace('%d', progress.percent));
+            }
+        })
+        .on('error', function (err) { // executes when the command errors
+            console.log('An error occurred: ' + err.message);
+        })
+        .on('end', function () { // executes when the command ends
+            let metrics = processVMAFLog(logPath);
+            console.log('VMAF: %d'.replace('%d', metrics.vmaf));
+        })
+        .input(base) // base video file
+        .input(encoded) // encoded video file
+        .addOption('-lavfi')
+        .addOption(filter) // add filter option
+        .addOption('-f', 'null') // pipe console output to a void
+        .output('-')
+        .run(); // run commmand
+}
+
+// psnr runs the ffmpeg psnr filter command
+function psnr(base, encoded, logPath, verbose) {
+    // build filter string
+    let filter = 'psnr=%s'.replace('%s', logPath);
+    // create raw command
+    let command = ffmpeg();
+    // define command and run
+    command.on('start', function (commandLine) { // executes when the command starts
+        console.debug('Spawned FFmpeg with command:\n' + chalk.default.blue(commandLine));
+    })
+        .on('progress', function (progress) { // executes when the command progresses
+            if (verbose === true) {
+                console.log('Processing PSNR: %d % done'.replace('%d', progress.percent));
+            }
+        })
+        .on('error', function (err) { // executes when the command errors
+            console.log('An error occured: ' + err.message);
+        })
+        .on('end', function (err, stdout, stderr) { // executes when the command ends
+            let regex = /y:([^\s]+)/;
+            let psnr = stdout.match(regex);
+            console.log('PSNR: %d'.replace('%d', psnr[1]));
+        })
+        .input(base) // base video file
+        .input(encoded) // encoded video file
+        .addOption('-lavfi')
+        .addOption(filter) // add filter option
+        .addOption('-f', 'null') // pipe console output to a void
+        .output('-')
+        .run() // run command
+}
+
+// processVMAFLog extracts the vmaf score from the vmaf log file
+function processVMAFLog(path) {
+    let data = fs.readFileSync(path);
+    let log = JSON.parse(data);
+
+    let vmaf = 0;
+    for (var frame of log.frames) {
         vmaf += frame.metrics.vmaf;
-        psnr += frame.metrics.psnr;
     }
 
     return {
         vmaf: vmaf / log.frames.length,
-        psnr: psnr / log.frames.length,
     };
 }
